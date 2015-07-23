435db9617e78cfe7562d64e3938ea1cfcadfc0a1
Richa Seth <rseth@luc.edu>
2015-07-23 17:08:55 -0500

    Adding final Result class

---
+++ LocusCourseSearch/src/com/richa/model/FinalResults.java
@@ -0,0 +1,91 @@
+package com.richa.model;
+
+/**
+ * 
+ * @author Richa
+ *
+ */
+public class FinalResults {
+	
+	String cluster;
+	int numberOfClasses;
+	String startTime;
+	String endTime;
+	String campus;
+	
+	public FinalResults(){
+		
+	}
+	public FinalResults(String cluster,
+	int numberOfClasses,
+	String startTime,
+	String endTime,
+	String campus){
+		this.cluster = cluster;
+		this.numberOfClasses = numberOfClasses;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.campus =  campus;
+		
+	}
+	/**
+	 * @return the cluster
+	 */
+	public String getCluster() {
+		return cluster;
+	}
+	/**
+	 * @param cluster the cluster to set
+	 */
+	public void setCluster(String cluster) {
+		this.cluster = cluster;
+	}
+	/**
+	 * @return the numberOfClasses
+	 */
+	public int getNumberOfClasses() {
+		return numberOfClasses;
+	}
+	/**
+	 * @param numberOfClasses the numberOfClasses to set
+	 */
+	public void setNumberOfClasses(int numberOfClasses) {
+		this.numberOfClasses = numberOfClasses;
+	}
+	/**
+	 * @return the startTime
+	 */
+	public String getStartTime() {
+		return startTime;
+	}
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	/**
+	 * @return the endTime
+	 */
+	public String getEndTime() {
+		return endTime;
+	}
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	/**
+	 * @return the campus
+	 */
+	public String getCampus() {
+		return campus;
+	}
+	/**
+	 * @param campus the campus to set
+	 */
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+}
