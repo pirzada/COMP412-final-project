50a6aef64d6e6faad088f60a1e78fc04d4b14fdd
Richa Seth <rseth@luc.edu>
2015-07-24 17:14:06 -0500

    Added comments in the model file

--- LocusCourseSearch/src/com/richa/model/FinalResults.java
+++ LocusCourseSearch/src/com/richa/model/FinalResults.java
@@ -7,15 +7,19 @@ package com.richa.model;
  */
 public class FinalResults {
 	
+	//Result to show clustered data
 	String cluster;
 	int numberOfClasses;
 	String startTime;
 	String endTime;
 	String campus;
 	
+	//Default Constructor
 	public FinalResults(){
 		
 	}
+	
+	//Constructor for mapping values
 	public FinalResults(String cluster,
 	int numberOfClasses,
 	String startTime,
@@ -28,60 +32,70 @@ public class FinalResults {
 		this.campus =  campus;
 		
 	}
+	
 	/**
 	 * @return the cluster
 	 */
 	public String getCluster() {
 		return cluster;
 	}
+	
 	/**
 	 * @param cluster the cluster to set
 	 */
 	public void setCluster(String cluster) {
 		this.cluster = cluster;
 	}
+	
 	/**
 	 * @return the numberOfClasses
 	 */
 	public int getNumberOfClasses() {
 		return numberOfClasses;
 	}
+	
 	/**
 	 * @param numberOfClasses the numberOfClasses to set
 	 */
 	public void setNumberOfClasses(int numberOfClasses) {
 		this.numberOfClasses = numberOfClasses;
 	}
+	
 	/**
 	 * @return the startTime
 	 */
 	public String getStartTime() {
 		return startTime;
 	}
+	
 	/**
 	 * @param startTime the startTime to set
 	 */
 	public void setStartTime(String startTime) {
 		this.startTime = startTime;
 	}
+	
 	/**
 	 * @return the endTime
 	 */
 	public String getEndTime() {
 		return endTime;
 	}
+	
 	/**
 	 * @param endTime the endTime to set
 	 */
 	public void setEndTime(String endTime) {
 		this.endTime = endTime;
 	}
+	
 	/**
 	 * @return the campus
 	 */
 	public String getCampus() {
 		return campus;
 	}
+	
 	/**
 	 * @param campus the campus to set
 	 */
