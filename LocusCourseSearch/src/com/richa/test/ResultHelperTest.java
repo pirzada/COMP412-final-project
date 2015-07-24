f7e72dcf350b3d542055c85d882260958fce6c2b
Richa Seth <rseth@luc.edu>
2015-07-24 05:30:34 -0500

    Adding Test cases

---
+++ LocusCourseSearch/src/com/richa/test/ResultHelperTest.java
@@ -0,0 +1,168 @@
+/**
+ * 
+ */
+package com.richa.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Richa
+ *
+ */
+public class ResultHelperTest {
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#readClinicCsvFile(java.lang.String, java.util.List)}.
+	 */
+	@Test
+	public final void testReadClinicCsvFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#filterBySearch(java.util.List, java.util.Map)}.
+	 */
+	@Test
+	public final void testFilterBySearch() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#filterByCluster(java.util.List)}.
+	 */
+	@Test
+	public final void testFilterByCluster() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#search(java.util.Map)}.
+	 */
+	@Test
+	public final void testSearch() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#getExpandedResult(java.util.Map)}.
+	 */
+	@Test
+	public final void testGetExpandedResult() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#getTime(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.richa.model.ResultHelper#getTimeOnly(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetTimeOnly() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
