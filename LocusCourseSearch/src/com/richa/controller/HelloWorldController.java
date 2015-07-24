00c753d9554ecd4c1377bd36524d1b6fe0b924e1
Richa Seth <rseth@luc.edu>
2015-07-24 05:29:53 -0500

    Complete Method

--- LocusCourseSearch/src/com/richa/controller/HelloWorldController.java
+++ LocusCourseSearch/src/com/richa/controller/HelloWorldController.java
@@ -10,36 +10,53 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.richa.model.FinalResults;
+import com.richa.model.Result;
 import com.richa.model.ResultHelper;
- 
+
 @Controller
-public class HelloWorldController{
- 
+public class HelloWorldController {
+
 	/**
 	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public ModelAndView handleRequestInternal() {
- 
+
 		ModelAndView model = new ModelAndView("form");
-		//model.addObject("msg", "hello world");
- 
+
 		return model;
 	}
-	
+
 	/**
 	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/result", method = RequestMethod.POST)
-	public ModelAndView handleSearchRequestInternal(@RequestParam Map<String, String> values ) {
-		
+	public ModelAndView handleSearchRequestInternal(@RequestParam Map<String, String> values) {
+
 		List<FinalResults> results = ResultHelper.search(values);
-	
+
 		ModelAndView model = new ModelAndView("result");
 		model.addObject("result", results);
- 
+		model.addObject("department", values.get("department"));
+		model.addObject("classType", values.get("radio"));
+		
+		return model;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/fullResult", method = RequestMethod.POST)
+	public ModelAndView handleExpandRequestInternal(@RequestParam Map<String, String> values) {
+
+		List<Result> results = ResultHelper.getExpandedResult(values);
+		System.out.println("size "+results.size());
+		ModelAndView model = new ModelAndView("fullResult");
+		model.addObject("result", results);
+
 		return model;
 	}
 }
